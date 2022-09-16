35f54484aafa9553a59dc07dce953ebd0fb84321
Jessica Serafini <jpitts_kiheihs@yahoo.com>
2022-09-15 21:52:34 -0400

    commit tree 32d3c786844d7af4dbbcfae6b346fda7363b936c

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
+const input = document.getElementById('button');
+
 function addingEventListener() {
+    function clickAlert() {
+        alert('I was clicked!');
+    }
+    
+    input.addEventListener('click', clickAlert);
+    
 }
+
+addingEventListener();
+
+const body = document.getElementsByTagName('body')[0];
+
+const secondChild = body.children[4];
+
+const div1 = secondChild.getElementsByTagName('div')[0];
+
+const div2 = div1.getElementsByTagName('div')[0];
+
+const div3 = div2.getElementsByTagName('div')[0];
+
+const div4 = div3.getElementsByTagName('div')[0];
+
+div4.addEventListener('click', clickAlert);
